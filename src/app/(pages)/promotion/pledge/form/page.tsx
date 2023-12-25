cd3f8bd06b0bc78157fa8d23e70b1d3d455cb74a
ohcho-dev <ohcho.dev@gmail.com>
2023-12-25 23:30:22 +0900

    add form info page

--- src/app/(pages)/promotion/pledge/form/page.tsx
+++ src/app/(pages)/promotion/pledge/form/page.tsx
@@ -1,6 +1,32 @@
-import { redirect } from "next/navigation";
+import BottomButton from "@/components/BottomButton";
+import Image from "next/image";
 
 export default function Page() {
-  redirect("/promotion/pledge/form/first");
-  return <div>엽서 만들기 질문 답변 페이지</div>;
+  return (
+    <div>
+      <h1 className="pt-[11.6rem] text-center text-[2.8rem]">
+        <b>
+          내가 생각하는 <span className="text-[#FD9D91]">배우자의 모습</span>을
+        </b>
+        <br /> 아래 선택지에서 골라주세요.
+      </h1>
+      <Image
+        className="mx-auto mt-[6.9rem] w-[33.1rem] h-[33.7rem]"
+        src="/info1.png"
+        alt="mock-up"
+        width={331}
+        height={337}
+      />
+
+      <span className="block mt-[7.2rem] text-center text-[2.1rem]">
+        아래 항목중 가장 비슷한 항목을 선택해주세요.
+      </span>
+
+      <BottomButton
+        label="확인하였습니다"
+        url="/promotion/pledge/form/first"
+        status={true}
+      />
+    </div>
+  );
 }
--- src/components/BottomButton.tsx
+++ src/components/BottomButton.tsx
@@ -1,21 +1,26 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 interface BottomButtonProps {
   label: string;
   status: boolean;
-  onClick: () => void;
+  onClick?: () => void;
+  url?: string;
 }
 export default function BottomButton({
   onClick,
+  url = "",
   label,
   status = false,
 }: BottomButtonProps) {
+  const router = useRouter();
   return (
     <button
       className={`w-[100vw] max-w-[518px] h-[8rem] text-[2.4rem] fixed bottom-0 ${
         status ? "bg-[#FFEBAA] text-black" : "bg-[#cccccc] text-white"
       }`}
-      onClick={onClick}
+      onClick={onClick ? onClick : () => router.push(url)}
       disabled={!status}
     >
       {label}
