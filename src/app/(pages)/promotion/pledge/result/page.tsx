13b85b7fd4fa8c14af08f98eb7c5be50eecc23d7
ohcho-dev <ohcho.dev@gmail.com>
2023-12-26 13:08:12 +0900

    test: 서약서 테스트

--- src/app/(pages)/promotion/pledge/result/page.tsx
+++ src/app/(pages)/promotion/pledge/result/page.tsx
@@ -7,13 +7,20 @@ export default function Page() {
   return (
     <div>
       <Toolbar />
-      <div className="mb-[10rem] max-h-calcResultPage overflow-auto scroll-m-0 ">
+      <div className="mb-[10rem] max-h-calcResultPage overflow-auto scroll-m-0">
         <Image
-          className="pt-[8rem] mx-auto "
+          className="pt-[8rem] mx-auto absolute"
           src="/paper.png"
           alt="서약서 배경"
           width={480}
           height={699}
+        />
+        <Image
+          className="pt-[8rem] px-[4rem] mx-auto absolute top-[4rem]"
+          src="/test.svg"
+          alt="서약서 내용"
+          width={600}
+          height={600}
         />
       </div>
       <BottomButton
