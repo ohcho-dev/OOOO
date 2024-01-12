fca3620f2e93a5fa9f0f69f6363359e9d7a5357d
ohcho-dev <ohcho.dev@gmail.com>
2024-01-12 18:03:49 +0900

    fix: QA

--- src/app/(pages)/promotion/pledge/page.tsx
+++ src/app/(pages)/promotion/pledge/page.tsx
@@ -26,7 +26,7 @@ export async function generateMetadata({
       title: "우리 가족 서약서 : 키워드 입력만으로 소중한 서약서 만들기️❤️",
       description: "아이의 목소리를 담은 유쾌한 약속",
       images: [
-        "https://oooo-pledge.s3.ap-northeast-2.amazonaws.com/image/meta_image_user_2.png",
+        "https://oooo-pledge.s3.ap-northeast-2.amazonaws.com/image/meta_image_user_1.png",
       ],
       url: "https://oooo-pi.vercel.app/promotion/pledge",
     },
